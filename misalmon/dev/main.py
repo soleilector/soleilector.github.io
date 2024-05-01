ce9b4f3c9050e48e588d2b983d7d6b6339d0b26c
sans evadere <144566342+soleilector@users.noreply.github.com>
2024-04-30 21:11:46 -0400

    Update main.py

--- misalmon/dev/main.py
+++ misalmon/dev/main.py
@@ -252,15 +252,18 @@ def getPage(pgName='Not Found'): #getpagename, argument based on ending of page
 
 def pageFromContent(pgContent,pgName='index',temp_header=HEADER,temp_content=CONTENT,temp_footer=FOOTER):
     # capitalize first letter of page's name
-    pgName = capTitle(pgName)
+    # pgName = capTitle(pgName)
     
     # generate page's html
     config_pgTitle = getConfig("custom_page_title") # do we want custom title?
     page_title = pgName
     if config_pgTitle == True:
-        page_title = ""
+        print("defining custom page title")
+        page_title = "<!-- predefined title removed -->"
+    else: # we want predetermined, auto-title
+        page_title = capTitle(pgName)
     
-    content_formatted =  transformPathKeys(temp_content.format(page_title=pgName,page_content=pgContent)) + getScripts()
+    content_formatted =  transformPathKeys(temp_content.format(page_title=page_title,page_content=pgContent)) + getScripts()
     header_formatted = getHeader(temp_header) # uses default static template embedded in file if no argument
     footer_formatted = getFooter(temp_footer) # uses default static template embedded in file if no argument
     print("AAAAGH")
